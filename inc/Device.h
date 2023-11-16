976b98fb3a5bd35cdf57b5dcc5cc813126a9c773
Alex <ales.cep@gmail.com>
2023-11-16 10:45:56 +0100

    Changing superclass by adding mqtt topic property

--- inc/Device.h
+++ inc/Device.h
@@ -5,10 +5,15 @@
 class Device
 {
   public:
+    Device(String topic) : _mqttTopic(topic){}
+    String Get_MQTT_topic() { return _mqttTopic; }
+
     virtual String Get_Current_State() = 0; //used to send current state of all devices to MQTT server
-    virtual String MQTT_Get_topic() = 0;      //MQTT topic used for publishing/subscribing to MQTT server
     virtual void MQTT_Message_Subscribe(String message) = 0;  //function that will be triggered when message for current topic is recevied
     virtual void MQTT_Message_Publish() = 0;  //function that will be publishing data to MQTT server
+
+  private:
+    String _mqttTopic;
 };
 
 #endif
