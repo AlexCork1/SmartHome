c38be2ea378eab02f4414481a1812fc9c0b2d51a
Alex <ales.cep@gmail.com>
2023-12-06 14:04:20 +0100

    code reorganization

--- inc/GasSensor.h
+++ inc/GasSensor.h
@@ -1,22 +1,23 @@
-
 #include "Device.h"
 
-class GasSensor : public Device
-{
-  public:
-    GasSensor(String topic, void (*ISR_GASSensor)());
+class GasSensor : public Device{
+public:
+    GasSensor(const char* topic, void (*ISR_GASSensor)());
 
-    //reset
+    String Get_Current_State() override;
+    void MQTT_Message_Subscribe(const String& message) override { /* NOTHING - it is only sending data. Check ino file */ }
+
     void Reset_Alarm();
     void Set_Alarm();
 
-    //functions dervied from Device class
-    String Get_Current_State();
-    void MQTT_Message_Subscribe(String message);
-    int32_t ReadState();
+    int32_t Read_State() const { return digitalRead(GAS_SENSOR_PIN); }
 
-  private:
-    bool gas_state;
+ private:
+    bool _gasState;
 
-    const int8_t GAS_SENSOR_PIN = 23;
+    static constexpr uint8_t GAS_SENSOR_PIN = 23;
+
+    static constexpr uint8_t JSON_BUFFER_SIZE = 20;
+    static constexpr const char* JSON_FORMAT = "{\"state\":%c}";  // JSON format for gas state
+    char jsonBuffer[JSON_BUFFER_SIZE];
 };
--- src/GasSensor.cpp
+++ src/GasSensor.cpp
@@ -7,15 +7,15 @@
  *
  */
 /*###########################################################################################################################################*/
-GasSensor::GasSensor(String topic, void (*ISR_GASSensor)()) :
-    Device(topic)
+GasSensor::GasSensor(const char* topic, void (*ISR_GASSensor)()) 
+    : Device(topic), _gasState(false)
 {
     pinMode(GAS_SENSOR_PIN, INPUT);
 
-    Reset_Alarm();
-
     // we will use isr approach
     attachInterrupt(digitalPinToInterrupt(GAS_SENSOR_PIN), ISR_GASSensor, CHANGE);
+    
+    Reset_Alarm();
 }
 
 /*###########################################################################################################################################*/
@@ -28,29 +28,16 @@ GasSensor::GasSensor(String topic, void (*ISR_GASSensor)()) :
 
 void GasSensor::Reset_Alarm()
 {
-    gas_state = false;
+    _gasState = false;
 }
 void GasSensor::Set_Alarm(){
-    gas_state = true;
+    _gasState = true;
 }
 
-/* return single LED state as String */
 String GasSensor::Get_Current_State()
 {
-    String respond("{\"gas\": \"");
-    respond += String(gas_state);
-    respond += "\"}";
-    return std::move(respond);
-}
-
-/* callback function that will be called when message with MQTT_Get_topic() is received */
-void GasSensor::MQTT_Message_Subscribe(String message)
-{
-    // NOTHING - it is only send data
-}
-
-int32_t GasSensor::ReadState(){
-    return digitalRead(GAS_SENSOR_PIN);
+    snprintf(jsonBuffer, JSON_BUFFER_SIZE, JSON_FORMAT, _gasState ? '1' : '0');
+    return String(jsonBuffer);
 }
 
 /*###########################################################################################################################################*/
