29233e7c99e41045d083a111a59006651468b0a7
Alex <ales.cep@gmail.com>
2023-12-13 12:04:25 +0100

    create file secrets.h

--- secrets.h
+++ secrets.h
@@ -1,6 +1,6 @@
 //data for access point connection
-const char* ssid = "WIFI";      //WiFi names
-const char* password = "WIFI_PASSWORD";	//WiFi password
+const char* ssid = "WiFi";      //WiFi names
+const char* password = "WifiPassword";	//WiFi password
 
 //data for MQTT server connection
 const char* mqtt_server = "broker.hivemq.com";
