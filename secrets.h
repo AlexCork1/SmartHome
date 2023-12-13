ef89d054a2a0aab80cfbb325ea4d5adbdaccec73
Alex <ales.cep@gmail.com>
2023-12-13 11:39:31 +0100

    Added secret.h file

---
+++ secrets.h
@@ -0,0 +1,8 @@
+//data for access point connection
+const char* ssid = "WIFI";      //WiFi names
+const char* password = "WIFI_PASSWORD";	//WiFi password
+
+//data for MQTT server connection
+const char* mqtt_server = "broker.hivemq.com";
+const char* mqtt_username = "smarthome_keyestudio";
+const char* mqtt_password = "smarthome_password_keyestudio";
