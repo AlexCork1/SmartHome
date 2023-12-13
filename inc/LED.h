3e9f5e5175fb9348a66003d6483b968576b9d96f
Alex <ales.cep@gmail.com>
2023-12-13 11:23:15 +0100

    removed virtual functions

--- inc/LED.h
+++ inc/LED.h
@@ -8,12 +8,6 @@ class LED : public Device
 {
   public:
     enum class LedState { Off, On };
-    LED(const char* topic) : Device(topic), _ledState(LedState::Off){}
-    virtual void On() = 0;
-    virtual void Off() = 0;
-    virtual void Toggle() = 0;
-
-  protected:
-    LedState _ledState;
+    LED(const char* topic) : Device(topic){}
 };
 #endif
